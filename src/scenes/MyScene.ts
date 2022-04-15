b3b590da62f9daf9477ce013f1fb65fa7ea844f3
Elfet <36362008+Elfet@users.noreply.github.com>
2022-04-15 19:58:42 +0900

    Feature/player-control プレイヤー生成 (#7)
    * 要件完了
    
    * リファクタリング

--- src/scenes/MyScene.ts
+++ src/scenes/MyScene.ts
@@ -1,35 +1,41 @@
 import Phaser from "phaser";
+import { Player } from "../objects/charactors/player";
 
 export class MyScene extends Phaser.Scene {
+  private player!: Player;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private staticInterior!: Phaser.Physics.Arcade.StaticGroup;
+
   constructor() {
     super({ key: 'myscene' });
   }
 
   preload() {
-    this.load.setBaseURL("https://labs.phaser.io");
-
-    this.load.image("sky", "assets/skies/space3.png");
-    this.load.image("logo", "assets/sprites/phaser3-logo.png");
-    this.load.image("red", "assets/particles/red.png");
+    this.load.image("map", "src/assets/maps/map_01.png")
+    this.load.image("table", "src/assets/maps/counter_table.png")
+    this.load.spritesheet(
+      "player",
+      "src/assets/characters/Chef_Alex_48x48.png",
+      { frameWidth: 48, frameHeight: 96}
+    )
   }
 
   create() {
-    this.add.image(400, 300, "sky");
+    // World生成
+    this.add.image(400, 300, "map").setScale(1.5);
+    this.physics.world.setBounds(90, 80, 622, 495);
+    this.cursors = this.input.keyboard.createCursorKeys();
 
-    const particles = this.add.particles("red");
+    // 固定オブジェクト生成
+    this.staticInterior = this.physics.add.staticGroup();
+    this.staticInterior.create(400, 339, "table").setScale(1.5).refreshBody();
 
-    const emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: "ADD",
-    });
+    // Player生成
+    this.player = new Player(this.physics, this.anims, 400, 200, 'player');
+    this.player.setCollider(this.staticInterior);
+  }
 
-    const logo = this.physics.add.image(400, 100, "logo");
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+  update() {
+    this.player.onDownPlayerBehavior(this.cursors);
   }
 }
