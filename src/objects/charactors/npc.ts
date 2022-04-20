368e6fa710d77b6ba1f8adcc215db17dcfd4bfba
yuthon <yh88gr4@gmail.com>
2022-04-20 15:48:40 +0900

    setTimeoutを使わないアニメーションの基本が完成

--- src/objects/charactors/npc.ts
+++ src/objects/charactors/npc.ts
@@ -13,6 +13,10 @@ export class Npc{
   private waitTime: number;
   // オーダー
   private order: string;
+  // 退店中
+  private isLeaving: boolean;
+
+  private didAnimationEnd: boolean;
   
   constructor(
     add: Phaser.GameObjects.GameObjectFactory,
@@ -24,11 +28,13 @@ export class Npc{
     this.tweens = tweens;
     this.isOnMove = false;
     this.isOnChair = false;
+    this.isLeaving = false;
     this.sprite.depth = 3;
     this.waitTime = 5000;
     this.NpcAnim = new NpcAnim(this.tweens);
     this.order = 'salmon_nigiri';
     this.orderRandom();
+    this.didAnimationEnd = true;
 
     // npcのアニメーション
     this.sprite.anims.create({
@@ -65,7 +71,16 @@ export class Npc{
       frameRate: 10,
     });
     
+    
   };
+
+  getIsLeaveing():boolean {
+    return this.isLeaving;
+  }
+
+  updateIsLeaving(state: boolean) {
+    this.isLeaving = state;
+  }
 
   getIsOnMove():boolean {
     return this.isOnMove;
@@ -92,13 +107,294 @@ export class Npc{
     this.sprite.depth = depth;
   };
 
-  walkToChair_0():void {
-    this.NpcAnim.walkToChair_0(this.sprite);
-  };
+  // walkToChair_0():void {
+  //   this.NpcAnim.walkToChair_0(this.sprite);
+  // };
+  sitDown (
+    
+  ):any {
+    return this.tweens.timeline({
+      tweens: [
+        {
+          onStart: ()=>{
+            
+          },
+          targets: this.sprite,
+          duration: 5000,
+          ease: 'Linear',
+          onComplete: ()=>{
+            console.log("hh")
+          }
+        }
+      ]
+    })
+  }
 
-  leaveChair_0():void {
-    this.NpcAnim.leaveFromChair_0(this.sprite);
-  };
+  walkToChair_0 (
+    function0: Function,
+    function1: Function,
+    function2: Function,
+    function3: Function,
+    function4: Function,
+    function5: Function,
+    function6: Function,
+    function7: Function,
+    function8: Function,
+    function9: Function,
+    function10: Function,
+    function11: Function,
+    function12: Function,
+    function13: Function,
+    function14: Function,
+  ):Phaser.Tweens.Timeline {
+    
+    return this.tweens.createTimeline({
+      paused: true,
+      tweens: [
+        {
+          onStart: ()=>{
+            this.didAnimationEnd = false;
+            function0();
+            function1();
+            function2();
+            function3();
+            this.sprite.play({key: 'walk_up', repeat: -1});
+          },
+          targets: this.sprite,
+          y: 420,
+          duration: 1500,
+          ease: 'Linear'
+        },
+        {
+          onStart: ()=>{
+            this.sprite.play({key: 'walk_left', repeat: -1});
+          },
+          targets: this.sprite,
+          x: 200,
+          duration: 2000,
+          ease: 'Linear'
+        },
+        {
+          onStart: ()=>{
+            this.sprite.play({key: 'walk_up', repeat: -1});
+          },
+          targets: this.sprite,
+          y: 320,
+          duration: 1000,
+          ease: 'Linear',
+          onComplete: ()=>{
+            function4();
+            function5();
+            function6();
+            function7();
+          }
+        },
+        // 座った
+        {
+          onStart: ()=>{
+            this.sprite.play({key: 'sit', repeat: -1});
+          },
+          targets: this.sprite,
+          y: 320,
+          duration: 5000,
+          ease: 'Linear',
+          onComplete: ()=>{
+            function8();
+            function9();
+            function10();
+            function11();
+          },
+        },
+        // 帰る
+        {
+          onStart: ()=>{
+            function12();
+            this.sprite.play({key: 'walk_down', repeat: -1});
+          },
+          targets: this.sprite,
+          y: 420,
+          duration: 1000,
+          ease: 'Linear'
+        },
+        {
+          onStart: ()=>{
+            this.sprite.play({key: 'walk_right', repeat: -1});
+          },
+          targets: this.sprite,
+          x: 430,
+          duration: 2000,
+          ease: 'Linear'
+        },
+        {
+          onStart: ()=>{
+            this.sprite.play({key: 'walk_down', repeat: -1});
+          },
+          targets: this.sprite,
+          y: 520,
+          duration: 1500,
+          ease: 'Linear',
+          onComplete: ()=>{
+            
+          }
+        },
+        {
+          onStart: ()=>{
+            
+          },
+          targets: this.sprite,
+          x: 360,
+          duration: 0,
+          ease: 'Linear',
+          onComplete: ()=>{
+            this.sprite.play({key: 'sit', repeat: -1});
+            function13();
+            function14();
+            this.didAnimationEnd = true;
+          }
+        }
+      ]
+    })
+  }
+
+  getDidAnimationEnd () {
+    return this.didAnimationEnd;
+  }
+
+  leaveChair_0 (
+    function0: Function,
+    function1: Function,
+    function2: Function,
+    function3: Function,
+    function4: Function,
+    function5: Function,
+    function6: Function,
+    // function7: Function,
+  ): Phaser.Tweens.Timeline {
+    return this.tweens.timeline({
+      tweens: [
+        {
+          onStart: ()=>{
+            function0();
+            function1();
+            function2();
+            function3();
+            function4();
+            this.sprite.play({key: 'walk_down', repeat: -1});
+          },
+          targets: this.sprite,
+          y: 420,
+          duration: 1000,
+          ease: 'Linear'
+        },
+        {
+          onStart: ()=>{
+            this.sprite.play({key: 'walk_right', repeat: -1});
+          },
+          targets: this.sprite,
+          x: 430,
+          duration: 2000,
+          ease: 'Linear'
+        },
+        {
+          onStart: ()=>{
+            this.sprite.play({key: 'walk_down', repeat: -1});
+          },
+          targets: this.sprite,
+          y: 520,
+          duration: 1500,
+          ease: 'Linear',
+          onComplete: ()=>{
+            
+          }
+        },
+        {
+          onStart: ()=>{
+            
+          },
+          targets: this.sprite,
+          x: 360,
+          duration: 0,
+          ease: 'Linear',
+          onComplete: ()=>{
+            this.sprite.play({key: 'sit', repeat: -1});
+            function5();
+            function6();
+          }
+        }
+      ]
+    })
+  }
+
+  forceLeaveChair_0 (
+    function0: Function,
+    function1: Function,
+    function2: Function,
+    function3: Function,
+    function4: Function,
+    function5: Function,
+    function6: Function,
+  ): Phaser.Tweens.Timeline {
+    return this.tweens.createTimeline({
+      paused: true,
+      tweens: [
+        {
+          onStart: ()=>{
+            function0();
+            function1();
+            function2();
+            function3();
+            function4();
+            
+            this.sprite.play({key: 'walk_down', repeat: -1});
+          },
+          targets: this.sprite,
+          y: 420,
+          duration: 1000,
+          ease: 'Linear'
+        },
+        {
+          onStart: ()=>{
+            this.sprite.play({key: 'walk_right', repeat: -1});
+          },
+          targets: this.sprite,
+          x: 430,
+          duration: 2000,
+          ease: 'Linear'
+        },
+        {
+          onStart: ()=>{
+            this.sprite.play({key: 'walk_down', repeat: -1});
+          },
+          targets: this.sprite,
+          y: 520,
+          duration: 1500,
+          ease: 'Linear',
+          onComplete: ()=>{
+            
+          }
+        },
+        {
+          onStart: ()=>{
+            
+          },
+          targets: this.sprite,
+          x: 360,
+          duration: 0,
+          ease: 'Linear',
+          onComplete: ()=>{
+            this.sprite.play({key: 'sit', repeat: -1});
+            function5();
+            function6();
+            this.didAnimationEnd = true;
+          }
+        }
+      ]
+    })
+  }
+
+  // leaveChair_0():void {
+  //   this.NpcAnim.leaveFromChair_0(this.sprite);
+  // };
 
   walkToChair_1():void {
     this.NpcAnim.walkToChair_1(this.sprite);
